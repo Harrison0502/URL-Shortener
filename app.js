2625921858236affe9a8cb9a7cddd254f24a52f3
harrison0502 <harrison0502@gmail.com>
2023-06-27 01:28:48 +0800

    feat:add redirect back to the original web function

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.get('/',(req,res)=>{
   res.render('index')
 })
 
+//接收原始網址
 app.post('/url', (req, res) => {
   const originalURL = req.body.originalURL;
   URL.findOne({ originalURL })
@@ -47,6 +48,19 @@ app.post('/url', (req, res) => {
 
 })
 
+//導回原始網站
+app.get('/:shortenedURL',(req,res)=>{
+  const shortenedURL=req.params.shortenedURL
+  URL.findOne({ shortenedURL })
+    .then(existingURL => {
+      if (existingURL) {
+        res.redirect(existingURL.originalURL);
+      } else {
+        res.redirect('/'); // 如果找不到對應的短網址，可以導向首頁或其他處理方式
+      }
+    })
+    .catch(error => console.log(error));
+})
 
 app.listen(3000,()=>{
   console.log('App is running on http://localhost:3000')
