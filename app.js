013990b55d175f5b6ec66aea205eba6797ed19be
harrison0502 <harrison0502@gmail.com>
2023-06-26 14:21:13 +0800

    feat:add handlebars

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const exphbs = require('express-handlebars');
 const mongoose=require('mongoose')
 const app = express()
 
@@ -19,8 +20,11 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/',(req,res)=>{
-  res.send('This is a shortenURL!')
+  res.render('index')
 })
 app.listen(3000,()=>{
   console.log('App is running on http://localhost:3000')
