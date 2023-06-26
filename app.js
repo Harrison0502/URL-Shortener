95ee81edc1f5691a5cc713165dea2a990bfe50a1
harrison0502 <harrison0502@gmail.com>
2023-06-27 02:47:19 +0800

    refactor:'modify mongoose config'

--- app.js
+++ app.js
@@ -1,33 +1,17 @@
 const express = require('express')
 const exphbs = require('express-handlebars');
-const mongoose=require('mongoose')
-const app = express()
-const URL = require('./models/URL') // 載入 Todo model
+const URL = require('./models/URL')
 const bodyParser = require('body-parser')
 const generateURL = require('./lib/generateURL')
 const isValidURL = require('./lib/isValidURL')
+require('./config/mongoose')
 
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
+const app = express()
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 
+//首頁路由
 app.get('/',(req,res)=>{
   res.render('index')
 })
